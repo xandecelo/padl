f8083de7a5215ddc235b4876ebd50d4cccaffab6
Alexandre Marcelo <alxmarcelo@gmail.com>
2022-06-27 12:34:22 -0300

    yaml descriptor for database source

--- padlapp/app/src/main/java/org/alefzero/padl/core/model/DatabaseSourceConfig.java
+++ padlapp/app/src/main/java/org/alefzero/padl/core/model/DatabaseSourceConfig.java
@@ -1,17 +1,75 @@
 package org.alefzero.padl.core.model;
 
-public class DatabaseSourceConfig extends PadlSourceConfig {
-    
-    public static final String TYPE = "database";
+import java.util.Set;
 
-    @Override
-    public String toString() {
-        return super.toString() + " - database config";
-    }
+public class DatabaseSourceConfig extends PadlSourceConfig {
+
+    public static final String TYPE = "database";
 
     @Override
     public String getType() {
         return TYPE;
+    }
+
+    private String jdbcUrl;
+    private String dbUsername;
+    private String dbPassword;
+    private String query;
+    private Set<String> datamap;
+    private Set<String> objectClasses;
+
+    public String getJdbcUrl() {
+        return jdbcUrl;
+    }
+
+    public void setJdbcUrl(String jdbcUrl) {
+        this.jdbcUrl = jdbcUrl;
+    }
+
+    public String getDbUsername() {
+        return dbUsername;
+    }
+
+    public void setDbUsername(String dbUsername) {
+        this.dbUsername = dbUsername;
+    }
+
+    public String getDbPassword() {
+        return dbPassword;
+    }
+
+    public void setDbPassword(String dbPassword) {
+        this.dbPassword = dbPassword;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public Set<String> getDatamap() {
+        return datamap;
+    }
+
+    public void setDataMap(Set<String> datamap) {
+        this.datamap = datamap;
+    }
+
+    public Set<String> getObjectClasses() {
+        return objectClasses;
+    }
+
+    public void setObjectClasses(Set<String> objectClasses) {
+        this.objectClasses = objectClasses;
+    }
+
+    @Override
+    public String toString() {
+        return "DatabaseSourceConfig [datamap=" + datamap + ", dbPassword=*****, dbUsername=" + dbUsername
+                + ", jdbcUrl=" + jdbcUrl + ", objectClasses=" + objectClasses + ", query=" + query + "]";
     }
 
 }
