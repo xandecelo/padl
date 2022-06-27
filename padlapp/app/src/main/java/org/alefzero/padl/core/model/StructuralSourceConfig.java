7613fb9f9256cbb8d2fa482ae8d748c0af447285
Alexandre Marcelo <alxmarcelo@gmail.com>
2022-06-27 15:37:03 -0300

    structural source service

--- padlapp/app/src/main/java/org/alefzero/padl/core/model/StructuralSourceConfig.java
+++ padlapp/app/src/main/java/org/alefzero/padl/core/model/StructuralSourceConfig.java
@@ -1,17 +1,51 @@
 package org.alefzero.padl.core.model;
 
-public class StructuralSourceConfig extends PadlSourceConfig {
-    
-    public static final String TYPE = "structural";
+import java.util.Set;
 
-    @Override
-    public String toString() {
-        return super.toString() + " - structural config";
-    }
+public class StructuralSourceConfig extends PadlSourceConfig {
+
+    public static final String TYPE = "structural";
 
     @Override
     public String getType() {
         return TYPE;
+    }
+
+    private String ldapType;
+    private String value;
+    private Set<String> attributes;
+    private Set<String> objectClasses;
+
+    public String getLdapType() {
+        return ldapType;
+    }
+
+    public void setLdapType(String ldapType) {
+        this.ldapType = ldapType;
+    }
+
+    public Set<String> getObjectClasses() {
+        return objectClasses;
+    }
+
+    public void setObjectClasses(Set<String> objectClasses) {
+        this.objectClasses = objectClasses;
+    }
+
+    public Set<String> getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(Set<String> attributes) {
+        this.attributes = attributes;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
     }
 
 }
--- padlapp/app/src/main/java/org/alefzero/padl/sources/StructuralSource.java
+++ padlapp/app/src/main/java/org/alefzero/padl/sources/StructuralSource.java
@@ -3,13 +3,18 @@ package org.alefzero.padl.sources;
 import java.lang.invoke.MethodHandles;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 import org.alefzero.padl.core.exceptions.PadlException;
 import org.alefzero.padl.core.model.PadlSourceConfig;
 import org.alefzero.padl.core.model.StructuralSourceConfig;
 import org.alefzero.padl.core.services.PadlSource;
 import org.alefzero.padl.core.services.PadlTarget;
+import org.alefzero.padl.utils.LdapUtils;
+import org.apache.directory.api.ldap.model.entry.Attribute;
+import org.apache.directory.api.ldap.model.entry.DefaultAttribute;
 import org.apache.directory.api.ldap.model.entry.Entry;
+import org.apache.directory.api.ldap.model.exception.LdapException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,7 +23,7 @@ public class StructuralSource extends PadlSource {
     final static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     private static final String ID = "structural";
-    private PadlSourceConfig config;
+    private StructuralSourceConfig config;
     private PadlTarget target;
 
     @Override
@@ -60,8 +65,21 @@ public class StructuralSource extends PadlSource {
 
     @Override
     protected void loadToTarget() throws PadlException {
-        // TODO Auto-generated method stub
-        
+        logger.debug("Processing structural element...");
+        List<Attribute> attributes = new LinkedList<Attribute>();
+        if (null != config.getAttributes()) {
+            for (String attribute : config.getAttributes()) {
+                StringTokenizer stEqual = new StringTokenizer(attribute, "=");
+                String key = stEqual.nextToken().trim();
+                String value = stEqual.countTokens() == 0 ? key : stEqual.nextToken().trim();
+                attributes.add(new DefaultAttribute(key, value));
+            }
+        }
+        try {
+            target.addEntry(LdapUtils.createEntry(config.getDn(), config.getLdapType(), config.getValue(), config.getObjectClasses(), attributes));
+        } catch (LdapException e) {
+            throw new PadlException(e);
+        }
     }
     
 }
